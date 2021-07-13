4ba3b25fa06eb62c4b199e833433f7ff5818343a
emasdeu <emasdeu@bcn3d.com>
2021-07-13 08:49:12 +0000

    Sync with cura-build-environment

--- projects/python-requests.cmake
+++
@@ -1,9 +0,0 @@
-ExternalProject_Add(PythonRequests
-    URL https://github.com/requests/requests/archive/v2.22.0.tar.gz
-    URL_MD5 b32e3383fbcd173e515883b6631a2eed
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${Python3_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${Python3_EXECUTABLE} setup.py install --single-version-externally-managed --record=requests-install.log
-    BUILD_IN_SOURCE 1
-)
-SetProjectDependencies(TARGET PythonRequests DEPENDS Python)
